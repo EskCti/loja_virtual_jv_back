b0cb3abaede288b9c472b733786d39f11468a2c9
eskokado <eskokado@gmail.com>
2023-11-12 16:48:43 -0300

    feat: create models Acesso, CategoriaProduto, Endereço, MarcaProduto, Pessoa, PessoaFisica and PessoaJuridica

---
+++ src/main/java/com/eskcti/loja_virtual_jv_back/models/PessoaFisica.java
@@ -0,0 +1,38 @@
+package com.eskcti.loja_virtual_jv_back.models;
+
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "pessoa_fisica")
+@PrimaryKeyJoinColumn(name = "id")
+public class PessoaFisica extends Pessoa {
+
+	private static final long serialVersionUID = 1L;
+
+	@CPF(message = "CPF está inválido")
+	@Column(nullable = false)
+	private String cpf;
+
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+}
