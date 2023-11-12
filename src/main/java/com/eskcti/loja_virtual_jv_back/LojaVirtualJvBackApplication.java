ca4fb7c70ce2459bb038fd0ca620c6abdf17b9b1
eskokado <eskokado@gmail.com>
2023-11-12 11:19:16 -0300

    first commit

---
+++ src/main/java/com/eskcti/loja_virtual_jv_back/LojaVirtualJvBackApplication.java
@@ -0,0 +1,13 @@
+package com.eskcti.loja_virtual_jv_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualJvBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualJvBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eskcti/loja_virtual_jv_back/LojaVirtualJvBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eskcti.loja_virtual_jv_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualJvBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
