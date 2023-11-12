f7d9978057de80f1c0c6c0e097a6f531ee130225
eskokado <eskokado@gmail.com>
2023-11-12 16:47:48 -0300

    feat: create enums TipoEndereco and TipoPessoa

---
+++ src/main/java/com/eskcti/loja_virtual_jv_back/enums/TipoPessoa.java
@@ -0,0 +1,28 @@
+package com.eskcti.loja_virtual_jv_back.enums;
+
+public enum TipoPessoa {
+	
+	JURIDICA("Jurídica"),
+	JURIDICA_FORNECEDOR("Jurídica e Fornecedor"),
+	FISICA("Física");
+	
+	private String descricao;
+	
+	
+	private TipoPessoa(String descrica) {
+		this.descricao = descrica;
+		
+	}
+	
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return this.descricao;
+	}
+
+}
