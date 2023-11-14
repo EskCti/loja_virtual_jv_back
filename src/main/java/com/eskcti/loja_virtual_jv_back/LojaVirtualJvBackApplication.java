8582afcba22a2c2dfbc87f62aa301ebce1bc3ee6
eskokado <eskokado@gmail.com>
2023-11-14 18:17:08 -0300

    feat: add annotation LojaVirtualJvBackApplication

--- src/main/java/com/eskcti/loja_virtual_jv_back/LojaVirtualJvBackApplication.java
+++ src/main/java/com/eskcti/loja_virtual_jv_back/LojaVirtualJvBackApplication.java
@@ -2,8 +2,22 @@ package com.eskcti.loja_virtual_jv_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
+@EntityScan(basePackages = "com.eskcti.loja_virtual_jv_back.models")
+@ComponentScan(basePackages = {"com.eskcti.loja_virtual_jv_back.*"})
+@EnableJpaRepositories(basePackages = {"com.eskcti.loja_virtual_jv_back.repositories"})
+@EnableTransactionManagement
+@EnableWebMvc
 public class LojaVirtualJvBackApplication {
 
 	public static void main(String[] args) {
