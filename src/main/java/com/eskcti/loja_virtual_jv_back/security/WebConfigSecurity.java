eca0f326765cbfc6b03b994c7f27e65c12fdbe46
eskokado <eskokado@gmail.com>
2023-12-04 17:13:55 -0300

    feat: create WebConfigSecurity

---
+++ src/main/java/com/eskcti/loja_virtual_jv_back/security/WebConfigSecurity.java
@@ -0,0 +1,35 @@
+package com.eskcti.loja_virtual_jv_back.security;
+
+import com.eskcti.loja_virtual_jv_back.services.ImplementacaoUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.servlet.http.HttpSessionListener;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter implements HttpSessionListener {
+
+    @Autowired
+    private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(implementacaoUserDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.GET, "/salvarAcesso", "deleteAcesso")
+                .antMatchers(HttpMethod.POST, "/salvarAcesso", "deleteAcesso");
+    }
+}
